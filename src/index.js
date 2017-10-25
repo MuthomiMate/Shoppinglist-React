22cd826902bbffd6368bc0382d28c469cd546095
Muthomi Mate <muthomimate@gmail.com>
2017-10-25 11:52:12 +0300

    [chore]add routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-i
+import Register from './Register'
 import registerServiceWorker from './registerServiceWorker';
-
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Login from "./Login";
+import Dashboard from "./dashboard"
 
 ReactDOM.render(
 <Router>
+    <Switch>
+        <Route exact path = "/" component ={Register}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/dashboard" component={Dashboard}/>
+    </Switch>
 </Router>, document.getElementById('root'),
 );
 registerServiceWorker();
