a86634be307e8461b397a76ecd59a70924ae4575
Muthomi Mate <muthomimate@gmail.com>
2017-11-14 15:11:19 +0300

    [Chore]add name to local storage after login

--- src/Login.js
+++ src/Login.js
@@ -58,11 +58,12 @@ class Login extends  Component{
             .then((response) => {
             if (response.data.message == "You logged in successfully."){
                 console.log(payload);
-                console.log(response.data.access_token);
                 window.localStorage.setItem('token', response.data.access_token);
+                window.localStorage.setItem('name', response.data.name);
                 this.props.history.push("/dashboard");
             }else{
                 console.log(response.data)
+                this.props.history.push("/login");
             }
 
 
@@ -75,6 +76,7 @@ class Login extends  Component{
                 //     alert(error.response.data.message)
                 // }
                 // this.props.history.push("/dashboard");
+                this.props.history.push("/login");
             });
     }
 }
