4e3622a0bec8867b3d4caf03806f8e0c64c565cb
Muthomi Mate <muthomimate@gmail.com>
2017-11-14 15:15:20 +0300

    [chore]add items to navbar

--- src/navbar.js
+++ src/navbar.js
@@ -2,25 +2,39 @@ import React, {Component} from 'react';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 class MainNav extends Component {
+    constructor(props){
+        super(props);
+        this.logout= this.logout.bind(this)
+
+
+    }
+
+    logout = (event) =>
+    {
+        window.localStorage.removeItem('token')
+
+    }
+
     render() {
+        const name= window.localStorage.getItem('name')
         return (
             <Navbar inverse collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#">React-Bootstrap</a>
+                        <a href="#">shopping List React</a>
                     </Navbar.Brand>
                 </Navbar.Header>
                 <Nav>
-                    <NavItem eventKey={1} href="#">Link</NavItem>
-                    <NavItem eventKey={2} href="#">Link</NavItem>
-                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                        <MenuItem divider />
-                        <MenuItem eventKey={3.4}>Separated link</MenuItem>
+
+                    {/*<NavItem eventKey={2} href="#">Link</NavItem>*/}
+                    <NavDropdown eventKey={3} title={name} id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}>change password</MenuItem>
+                        {/*<MenuItem eventKey={3.2}>Another action</MenuItem>*/}
+                        {/*<MenuItem eventKey={3.3}>Something else here</MenuItem>*/}
                     </NavDropdown>
+                    <NavItem className="nav-link" href="#">Logout</NavItem>
                 </Nav>
+
             </Navbar>
         )
     }
