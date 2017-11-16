9aafac05c32819b74c3b38fa8d378718cc458340
Muthomi Mate <muthomimate@gmail.com>
2017-11-16 17:27:24 +0300

    [chore]add axios then test

--- src/tests/resetPassword.test.js
+++ src/tests/resetPassword.test.js
@@ -3,7 +3,7 @@ import PassReset from '../resetPassword'
 import ReactDOM from 'react-dom'
 import Enzyme from 'enzyme'
 import {toast} from 'react-toastify'
-import Toaster from '../sucessToaster'
+import Toaster from '../sucessToaster';
 import {shallow,mount} from 'enzyme'
 import moxios from 'moxios'
 import Adapter from 'enzyme-adapter-react-16';
@@ -23,36 +23,41 @@ describe ('<PassReset/>', () =>{
         input.simulate('change', { target });
         expect(wrapper.state().email).toEqual(target.value);
     })
+    describe ('mocking axios requests', function () {
+        beforeEach(function () {
+            moxios.install()
+        })
+        afterEach(function () {
+            moxios.uninstall()
+        })
+        it('returns the responses according to inputs', function (done) {
 
-})
+            const wrapper = mount(<PassReset />);
+            const form = wrapper.find('form');
+            const input = wrapper.find('#email')
+            const event  =
+                {target:{
+                    value: 'muthomi@gmail.com',
+                }};
+            input.simulate('change', event)
+            form.simulate('submit', { preventDefault() {} })
+            moxios.wait(function () {
+                let request = moxios.requests.mostRecent()
+                request.respondWith({
+                    status: 200,
+                    response :{message:"The email has been sent to your email"}
+                })
+                    .then(function () {
+                        toast.success(response.data.message) ;
 
-
-describe ('mocking axios requests', function () {
-    beforeEach(function () {
-        moxios.install()
-    })
-    afterEach(function () {
-        moxios.uninstall()
-    })
-    it('returns the responses according to inputs', function () {
-
-        const wrapper = mount(<PassReset />);
-        const input = wrapper.find('#button');
-        const target = {
-            value: 'muthomi@gmail.com',
-        };
-        input.simulate('click')
-
-    })
-moxios.wait(function () {
-    let request = moxios.requests.mostRecent()
-    request.respondWith({
-        status: 200,
-        response :{message:"The email has been sent to your email"}
-    })
-        .then(function () {
-            expect( toast.success(response.data.message).toEqual("The email has been sent to you")  )
+                    })
+                done()
+            })
 
         })
+
+    })
+
 })
-})
+
+
