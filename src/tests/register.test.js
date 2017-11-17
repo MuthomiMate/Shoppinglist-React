159c2c21129451f1495dac621403e9b95bf595d1
Muthomi Mate <muthomimate@gmail.com>
2017-11-17 16:22:56 +0300

    [chore]test all states on register

--- src/tests/register.test.js
+++ src/tests/register.test.js
@@ -14,7 +14,7 @@ describe('<Register/>', () =>{
         const div = document.createElement('div')
         ReactDOM.render(<Register/>, div)
     })
-    it('changes all the states', () => {
+    it('changes first name state', () => {
         const wrapper = mount(<Register/>);
         const fname = wrapper.find('input#fname');
         const lname = wrapper.find('input#lname');
@@ -23,33 +23,46 @@ describe('<Register/>', () =>{
         const target = {
             value: 'muthomi'
         };
-        const target2 = {
+
+        fname.simulate('change', {target})
+        expect(wrapper.state().first_name).toEqual(target.value);
+
+    })
+    it('changes last name state', () => {
+        const wrapper = mount(<Register/>);
+        const lname = wrapper.find('input#lname');
+        const target = {
             value: 'mate',
         };
-        const target3 = {
-            value: 'muthomi@gmail.com',
+        lname.simulate('change', { target });
+        expect(wrapper.state().last_name).toEqual(target.value);
+    })
+    it('changes last name state', () => {
+        const wrapper = mount(<Register/>);
+        const lname = wrapper.find('input#lname');
+        const target = {
+            value: 'mate',
         };
-        const target4 = {
+        lname.simulate('change', { target });
+        expect(wrapper.state().last_name).toEqual(target.value);
+    })
+    it('changes email state', () => {
+        const wrapper = mount(<Register/>);
+        const email = wrapper.find('input#email');
+        const target = {
+            value: 'muthomimate@gmail.com',
+        };
+        email.simulate('change', { target });
+        expect(wrapper.state().email).toEqual(target.value);
+    })
+    it('changes password state', () => {
+        const wrapper = mount(<Register/>);
+        const pass = wrapper.find('input#password');
+        const target = {
             value: 'pass1234',
         };
-
-        fname.simulate('change', {target});
-
-        email.simulate('change', { target3 });
-        password.simulate('change', { target4 });
-
-        console.log(fname.html())
-        console.log(lname.html())
-        console.log(email.html())
-        console.log(password.html())
-
-        console.log(wrapper.state())
-        expect(wrapper.state().first_name).toEqual(target.value);
-        // lname.simulate('change', { target2 });
-        // expect(wrapper.state().last_name).toEqual(target2.value);
-        // expect(wrapper.state().email).toEqual(target3.value);
-        // expect(wrapper.state().password).toEqual(target4.value);
-
+        pass.simulate('change', { target });
+        expect(wrapper.state().password).toEqual(target.value);
     })
 })
 
