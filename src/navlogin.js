4ceba4aa40a0c861e5fa1289745e339cd0722214
Muthomi Mate <muthomimate@gmail.com>
2017-12-04 17:42:36 +0300

    [chore #153117312]create a login and register navbar

---
+++ src/navlogin.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const NavLogin = ()=>{
+    return (
+        <div>
+            <nav className="navbar navbar-inverse bg-inverse">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a className="navbar-brand" href="#">shopping list react</a>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className='Navbar-nav' style={{float:"right"}}>
+                            <li className="nav-item">
+                                <a className="nav-link" href={'/login'}>Login</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href={'/'}>Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+export default NavLogin
