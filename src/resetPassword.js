594eb1b493c1944b18edbaf72bb0a0eeeb1ff098
Muthomi Mate <muthomimate@gmail.com>
2017-12-07 12:40:16 +0300

    [chore #153117312]add spinner to show before request is processed

--- src/resetPassword.js
+++ src/resetPassword.js
@@ -53,7 +53,8 @@ class PassReset extends Component {
                                     <input type="email" required className="form-control" id="email" placeholder="Enter your Email"onChange={(event)=>this.setState({email:event.target.value})}/>
                                 </div>
                                 <div className="form-group">
-                                    <input type="submit" value="submit" className="btn btn-primary" id="button" />
+                                    {this.state.showspinner ? <LoadingSpinner/>:
+                                    <input type="submit" value="submit" className="btn btn-primary" id="button" />}
                                 </div>
                             </form>
                         </div>
--- src/shoppingItems.js
+++ src/shoppingItems.js
@@ -6,6 +6,7 @@ import Toaster from './sucessToaster'
 import {toast} from 'react-toastify'
 import {} from './helperfunctions'
 import {PromError} from "./helperfunctions";
+import LoadingSpinner from './spinner'
 import {BaseUrl, PrevAndNextStates} from "./helperfunctions";
 
 const token = "Bearer "+window.localStorage.getItem('token');
@@ -21,6 +22,7 @@ class ShoppingItems extends Component {
             prev: "",
             search: "",
             page: "",
+            showspinner: false,
             items: []
 
         }
@@ -31,9 +33,13 @@ class ShoppingItems extends Component {
 
     }
     componentDidMount(){
+        //on component mount call the get items function
         this.getItems()
     }
     getItems = () =>{
+        //function to get items for a particular shopping list
+        this.setState({showspinner:true})
+        //sending the request
         axios({
             url: `${BaseUrl()}shoppinglists/${this.props.match.params.id}/items/`,
             method: `GET`,
@@ -45,19 +51,27 @@ class ShoppingItems extends Component {
         })
 
             .then((response) => {
+                //the request is successful
+                this.setState({showspinner:false})
                 console.log(response.data);
                 if(response.data!=[]){
                     this.setState({msg: response.data.message })
                 }
+                //call the previous and next states function to  set the state
                 PrevAndNextStates(response, this)
                 this.setState({items: response.data.shopping_lists})
+
             })
             .catch((error) => {
+                //the request is not successful call the promise error function
+                this.setState({showspinner:false})
                 PromError(error, this)
+
             })
 
     };
     getItemsnext = () =>{
+        //gets the next page of the shopping list if next button is clicked
         console.log(this.props.id);
         axios({
             url: `${BaseUrl()}shoppinglists/${this.props.match.params.id}/items/${this.state.next}`,
@@ -70,19 +84,24 @@ class ShoppingItems extends Component {
         })
 
             .then((response) => {
+                //the request was successful
                 console.log(response.data);
                 if(response.data!=[]){
                     this.setState({msg: response.data.message })
                 }
+                //call the next and previous states function
                 PrevAndNextStates(response, this)
                 this.setState({items: response.data.shopping_lists})
             })
             .catch((error) => {
+                //request not successful
+                //call the promise error function
                 PromError(error, this)
             })
 
     };
     getItemsprev = () =>{
+        //gets the previous page items if button for previous is called
         console.log(this.props.id);
         axios({
             url: `${BaseUrl()}shoppinglists/${this.props.match.params.id}/items/${this.state.prev}`,
@@ -93,21 +112,26 @@ class ShoppingItems extends Component {
             },
 
         })
-
             .then((response) => {
+                //request is successfull
                 console.log(response.data);
                 if(response.data!=[]){
                     this.setState({msg: response.data.message })
                 }PrevAndNextStates(response, this)
+                //set items state to the ones that have been returned
                 this.setState({items: response.data.shopping_lists})
             })
             .catch((error) => {
+                //request not successful
+                //call the promise error function
                 PromError(error, this)
             })
 
     };
     searchitem =() => {
+        //function to search items and also determine the items to display per page
         let urllink="";
+        //make the url according to the states of search and page limit
         if(this.state.page=== "" && this.state.search===""){
             urllink = `${BaseUrl()}shoppinglists/${this.props.match.params.id}/items/`
         }
@@ -122,6 +146,7 @@ class ShoppingItems extends Component {
         }
         console.log(urllink)
         axios({
+            //send the request
             url: urllink,
             method: 'GET',
             headers : {
@@ -130,7 +155,7 @@ class ShoppingItems extends Component {
             },
         })
             .then((response) => {
-                console.log(response)
+                //request is successful
                 if(response.data.message==="Shopping list name does not exist") {
                     this.setState({msg: response.data.message})
 
@@ -141,14 +166,18 @@ class ShoppingItems extends Component {
                 }
             })
             .catch((error) => {
+                //request not successful call the promise error function
                 PromError(error, this)
             })
     }
     addItem = () =>{
+        //function to send the request for adding items
+        //send name as payload for the request
         let payload = {
             'name': this.state.name
         };
         axios({
+            //send tthe request
             url: `${BaseUrl()}shoppinglists/${this.props.match.params.id}/items/`,
             method : 'post',
             data: payload,
@@ -158,20 +187,26 @@ class ShoppingItems extends Component {
             }
         })
             .then((response) =>{
+                //request is successful
+                //notify the user that the item has been added
                 toast.success("Item has been added successfully")
                 var newStateArray = this.state.items.slice();
                 newStateArray.push(response.data);
                 this.setState({items: newStateArray});
             })
             .catch((error) =>{
+                //request not successfull, call the promise error function
                 PromError(error, this)
             })
     }
     editItem = () => {
+        //function to edit a shopping list
+        //send the new name as the payload
         let payload = {
             "name": this.state.name
         }
         axios({
+            //send the request
             url: `${BaseUrl()}shoppinglists/items/${this.state.id}`,
             method: 'PUT',
             data: payload,
@@ -181,20 +216,26 @@ class ShoppingItems extends Component {
             }
         })
             .then((response) => {
+                //request successful display success toaster
                 console.log(response.data)
                 toast.success("Item has been edited successfully")
+                //remove the item before from items state
                 this.setState({items:this.state.items.filter(items => items.id !== this.state.id )})
                 var newStateArray = this.state.items.slice();
+                //add the edited item to the state
                 newStateArray.push(response.data);
                 this.setState({items: newStateArray});
             })
             .catch((error) => {
+                //request not successful, calls the promise errors function
                 PromError(error, this)
             })
 
     }
     deleteItem = () => {
+        //function to delete an item
         axios({
+            //send the request
             url: `${BaseUrl()}shoppinglists/items/${this.state.id}`,
             method: 'DELETE',
             headers: {
@@ -203,11 +244,15 @@ class ShoppingItems extends Component {
             }
         })
             .then((response) => {
+                //request successfull
                 console.log(response.data)
                 toast.success(response.data.message)
+                //remove the deleted shopping list from the state
                 this.setState({items:this.state.items.filter(items => items.id !== this.state.id )})
             })
             .catch((error) => {
+                //request not successful
+                //call the promise error function
                 PromError(error, this)
             })
 
@@ -239,28 +284,39 @@ class ShoppingItems extends Component {
                                 <ReactBootstrap.Button bsStyle="primary" onClick={this.searchitem}>Go</ReactBootstrap.Button>
                             </div>
                         </div>
-                    <ReactBootstrap.Table responsive bordered className="sTable" style={{marginTop: '20px'}}>
-                        <thead className="bg-info">
-                        <tr>
-                            <th></th>
-                            <th>Item Name</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            items.map((items) => (
-                                <tr key = {items.id}>
-                                    <td><i>{++x}</i></td>
-                                    <td>{items.name}</td>
-                                    <td><div className= "button btn-success glyphicon glyphicon-pencil" data-toggle="modal" data-target="#edititem" onClick={(event => this.setState({id: items.id}))}></div></td>
-                                    <td><div className= "button btn-danger glyphicon glyphicon-trash" data-toggle="modal" data-target="#deleteitem" onClick={(event => this.setState({id: items.id}))}></div></td>
-                                </tr>
-                            ))
-                        }
-                        </tbody>
-                    </ReactBootstrap.Table></div>}
+                            {this.state.showspinner ? <LoadingSpinner/> :
+                                <ReactBootstrap.Table responsive bordered className="sTable"
+                                                      style={{marginTop: '20px'}}>
+                                    <thead className="bg-info">
+                                    <tr>
+                                        <th></th>
+                                        <th>Item Name</th>
+                                        <th>Edit</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        items.map((items) => (
+                                            <tr key={items.id}>
+                                                <td><i>{++x}</i></td>
+                                                <td>{items.name}</td>
+                                                <td>
+                                                    <div className="button btn-success glyphicon glyphicon-pencil"
+                                                         data-toggle="modal" data-target="#edititem"
+                                                         onClick={(event => this.setState({id: items.id}))}></div>
+                                                </td>
+                                                <td>
+                                                    <div className="button btn-danger glyphicon glyphicon-trash"
+                                                         data-toggle="modal" data-target="#deleteitem"
+                                                         onClick={(event => this.setState({id: items.id}))}></div>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                    </tbody>
+                                </ReactBootstrap.Table>
+                            }</div>}
 
                         <div style={{float:'right'}} >
                             {this.state.next?<div style={{padding: '5px', display: 'inline-block'}} onClick={this.getItemsnext}><ReactBootstrap.Button bsStyle="primary" >Next</ReactBootstrap.Button></div>: ""}
