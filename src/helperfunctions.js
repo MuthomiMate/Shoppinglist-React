9d15d219c961f0eca9491c2578b222015d30d86f
Muthomi Mate <muthomimate@gmail.com>
2017-12-07 12:21:00 +0300

    [chore #153117312]remove timeout wait before redirecting to login

--- src/helperfunctions.js
+++ src/helperfunctions.js
@@ -1,45 +1,48 @@
 import {toast} from 'react-toastify'
 export const PromError =(error,dashboard) => {
+    //function to handle the promise errors
      if(error.response) {
          if(error.response.data.message==="Expired token. Please login to get a new token"){
-         setTimeout(function(){
              window.localStorage.removeItem("token")
              window.localStorage.removeItem("name")
              dashboard.props.history.push("/login")
-         },3000);
          }
          if(error.response.data.message==="That shopping list does not exist" || error.response.data.message==="You do not have permission to view that shoppinglist"){
+             //go to error 404 page if one is not authorized to view the shopping list
              dashboard.props.history.push("*")
          }
+         //notify users the reason for failure
          toast.error(error.response.data.message)
 
      }
      else if(error.request){
+         //displays the internet errors in case there is no internet
          console.log(error.request)
          toast.error("There is no Internet Connection. Connect and try again")
      }
      else{
          console.log('Error', error.message)
      }
-     console.log(error.config)
  }
 
 export const BaseUrl = () => {
+    //function that returns base url
     const  apiBaseUrl  = 'https://shopping-list-api-muthomi.herokuapp.com/';
     return apiBaseUrl
 }
 
 export const PrevAndNextStates = (response, dashboard) => {
+    //function that sets next and previous page states
     if (response.data.previous_page !== "None"){
-        dashboard.setState({prev:response.data.previous_page});
+        dashboard.setState({prev: response.data.previous_page});
     }
     else{
-        dashboard.setState({prev:''});
+        dashboard.setState({prev:""});
     }
     if (response.data.next_page !== "None"){
         dashboard.setState({next:response.data.next_page});
     }else{
-        dashboard.setState({next:''});
+        dashboard.setState({next:""});
     }
 
 }
